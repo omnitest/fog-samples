6610790ee507a98f71590a986b75eabefb2eb1a8
Max Lincoln <max@devopsy.com>
2014-12-03 13:09:25 -0500

    Don't need mkfile, doesn't work on Ubuntu anyways

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ BASE_DIR="$(dirname "$SCRIPTS_DIR")"
 FIXTURE_DIR="$BASE_DIR/fixtures"
 
 # Create a file larger than 5GB for large file upload test
-mkfile -n 6g $FIXTURE_DIR/large_file.bin
+# mkfile -n 6g $FIXTURE_DIR/large_file.bin
 
 # Grab a sample website for the directory upload tests
 git clone https://github.com/thoughtworks/p2 $FIXTURE_DIR/sample_site/p2
